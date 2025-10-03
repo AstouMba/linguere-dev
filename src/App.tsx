c094d195ee44015ddffab0dbadbbcb0a2001e0e5
AstouMba <assalih1303@gmail.com>
2025-10-03 00:51:02 +0000

    modif

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/AppSidebar";
 import SnowEffect from "@/components/SnowEffect";
+import { AppSidebar } from "@/components/AppSidebar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
@@ -20,8 +20,8 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <Sonner />
       <SnowEffect />
+      <Sonner />
       <BrowserRouter>
         <SidebarProvider defaultOpen={true}>
           <div className="flex min-h-screen w-full">
